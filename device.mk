e9e586d5792f9dcb49f6fe57019a9a8ddfda2a93
rama982 <ramanarubp@gmail.com>
2021-01-04 12:29:42 +0100

    land: Update thermal blobs from Redmi 7
    * from Onclite PKQ1.181021.001/V10.2.5.0.PFLCNXM

--- device.mk
+++ device.mk
@@ -456,8 +456,7 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl:64 \
-    android.hardware.thermal@1.0-service \
-    thermal.msm8937
+    android.hardware.thermal@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
